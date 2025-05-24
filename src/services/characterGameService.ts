b9e888a9850cfcda1e6f964f44ae98b8c7a0196a
Ruben Berreguero <rubenbr85@gmail.com>
2025-05-25 00:44:01 +0200

    FIX: characterGameService api key

--- src/services/characterGameService.ts
+++ src/services/characterGameService.ts
@@ -1,4 +1,5 @@
 import { Character } from '../models/Character';
+import axios from 'axios';
 
 const CHARACTERS_LIMIT = 5;
 //const API_BASE_URL = 'https://lilu-memory-backend.vercel.app/api';
@@ -6,16 +7,15 @@ const API_BASE_URL = 'http://localhost:3000/api';
 
 export const fetchCharacters = async (sourceId: number, difficultyId: number): Promise<Character[]> => {
   try {
-    const response = await fetch(
-      `${API_BASE_URL}/characters?sourceId=${sourceId}&limit=${CHARACTERS_LIMIT}&difficultyId=${difficultyId}`
+    const response = await axios.get(
+      `${API_BASE_URL}/characters?sourceId=${sourceId}&limit=${CHARACTERS_LIMIT}&difficultyId=${difficultyId}`,
+      {
+        headers: {
+          'x-api-key': import.meta.env.VITE_API_KEY
+        }
+      }
     );
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data as Character[];
+    return response.data;
   } catch (error) {
     console.error('Error fetching characters:', error);
     throw error;
