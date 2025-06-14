756aac3cdf4518dede9d7a23563b157b7e422500
Ruben Berreguero <rubenbr85@gmail.com>
2025-06-15 01:01:14 +0200

    ADDED: implementación del hook useSources para la gestión de datos de fuentes utilizando @tanstack/react-query

--- src/services/sourceService.ts
+++ src/services/sourceService.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 import { CharacterSource } from '../models/CharacterSource';
 
 const API_URL = `${import.meta.env.VITE_API_URL}/api/sources`;
-
 
 export const fetchSources = async (): Promise<CharacterSource[]> => {
   try {
@@ -16,4 +16,13 @@ export const fetchSources = async (): Promise<CharacterSource[]> => {
     console.error('Error fetching sources:', error);
     throw error;
   }
+};
+
+export const useSources = () => {
+  return useQuery<CharacterSource[], Error>({
+    queryKey: ['sources'],
+    queryFn: fetchSources,
+    staleTime: 5 * 60 * 1000, // Los datos se consideran frescos por 5 minutos
+    gcTime: 30 * 60 * 1000, // Los datos se mantienen en caché por 30 minutos
+  });
 };
